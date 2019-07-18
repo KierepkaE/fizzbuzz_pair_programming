114d8a7e27fb765fd6278755ee962cb822cd7e00
Imane <imaneben8@gmail.com>
2019-07-18 15:19:06 +0100

    add third test for fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,5 +6,8 @@ describe 'fizzbuzz' do
   it 'returns "buzz" if number is divided by 5' do
     expect(fizzbuzz(20)).to eq 'buzz'
   end
+  it 'returns "fizzbuzz" if number is divided by 5 and 3' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 end
 
