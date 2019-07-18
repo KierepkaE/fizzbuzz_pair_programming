66dd7c9fcf4426c94fc8a8403277639d25fb6306
kierepkae <kierepkaedyta@gmail.com>
2019-07-18 15:06:16 +0100

    add second test for buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,10 @@
 require_relative '../lib/fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" if number is divided by 5' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  it 'returns "buzz" if number is divided by 5' do
+    expect(fizzbuzz(20)).to eq 'buzz'
   end
 end
 
